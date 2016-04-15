5191bcc432e55bf87e74e53d4dd8762b615ff2a2
ncistudent1 <ruth.stephenson1@gmail.com>
2016-04-15 17:32:00 +0000

    changed relationship

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,6 +1,6 @@
 class Bike < ActiveRecord::Base
 
-    has_one :rentals
+    has_one :rental
     
     def self.search(search_for)
         Bike.where("name = ?", search_for)
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,6 @@
 class Rental < ActiveRecord::Base
     belongs_to :user
     has_many :bikes
+    
+    #validates :rentalDuration, :range => 1..30
 end
